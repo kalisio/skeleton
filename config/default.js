c3a8b1fa29e69975b4a4510a9ca70cc6ad93f64b
cnouguier <ch.nouguier@gmail.com>
2024-04-05 18:40:36 +0200

    chore; replaced KTerms by KDocument

--- config/default.js
+++ config/default.js
@@ -81,7 +81,8 @@ module.exports = {
       id: 'terms-policies', 
       label: 'screen.TERMS_AND_POLICIES', 
       dialog: {
-        component: 'app/KTerms'
+        component: 'document/KDocument',
+        url: 'skeleton-terms.md'
       }
     }],
     logout: {
