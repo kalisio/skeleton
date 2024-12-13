a7aa90ab2f487d15b4a923d995461d4046c93e41
cnouguier <ch.nouguier@gmail.com>
2024-12-13 17:39:38 +0100

    feat: Remove custom service #14

--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,5 @@
 import logger from 'loglevel'
 import kdkCore from '@kalisio/kdk/core.client'
-import CustomService from './custom.service'
 
 export default async function () {
   const api = this
@@ -8,7 +7,6 @@ export default async function () {
   // Set up our plugin services
   try {
     await api.configure(kdkCore)
-    api.createService('custom', { service: CustomService })
   } catch (error) {
     logger.error(error.message)
   }
